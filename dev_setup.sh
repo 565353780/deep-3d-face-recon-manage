46717a6e55c33ac1f397ba3a9441e1d89946dc1f
chLi <565353780@qq.com>
2022-11-03 18:57:37 +0800

    update setup bash files

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,5 +1,7 @@
 cd ..
 git clone https://github.com/sicxu/Deep3DFaceRecon_pytorch.git
+git clone https://github.com/NVlabs/nvdiffrast
+git clone https://github.com/deepinsight/insightface.git
 
 pip install numpy scikit-image scipy pillow ipython pyyaml \
   matplotlib opencv-python kornia dominate trimesh
@@ -9,4 +11,10 @@ pip install torch torchvision torchaudio \
 
 pip install tensorboard==1.15.0
 pip install tensorflow==1.15.0
+
+cd nvdiffrast
+pip install .
+cd ..
+
+cp -r ./insightface/recognition/arcface_torch ./Deep3DFaceRecon_pytorch/models/
 
