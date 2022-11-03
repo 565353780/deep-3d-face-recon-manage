d1daf0650ad2012720080bae777c28613cf0efb8
chLi <565353780@qq.com>
2022-11-03 20:11:54 +0800

    remove sample_image folder, use my test folder

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
 python test.py \
   --name=face_recon \
   --epoch=20 \
-  --img_folder=/home/chli/chLi/Deep3DFaceRecon/faces/fufu/
-  # --img_folder=./example_image/
+  --img_folder=/home/chli/chLi/Deep3DFaceRecon/faces/
 
